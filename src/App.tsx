e9188dfa4cd512169c7e5e6e633344e7719f41bf
Brandon Pessman <pessman.brandon@gmail.com>
2024-05-31 03:19:42 -0500

    add env vars to react and change page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useEffect } from 'react'
+import * as settings from './constants/settings'
 
 function App() {
     useEffect(() => {
+        // Set Page Title
+        document.title = settings.PROJECT_NAME || document.title
+
         // Load Data through promise
         // If found, set state correctly and patch/update
         // If didn't find, set default save
     }, [])
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <h1>{settings.PROJECT_NAME}</h1>
+            <h2>{settings.PROJECT_VERSION}</h2>
         </div>
     )
 }
--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,5 +1,12 @@
-export const PROJECT_NAME: string = 'PRESTIGE-FRAMEWORK'
-export const PROJECT_VERSION: string = 'v0.0.1'
+const toTitleCase = (str: string) => {
+    console.log(str)
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    })
+}
+
+export const PROJECT_NAME: string | undefined = toTitleCase(process.env.REACT_APP_NAME?.replaceAll('-', ' ') || 'Undefined Name')
+export const PROJECT_VERSION: string | undefined = process.env.REACT_APP_VERSION
 
 export const AUTO_SAVE_TIMER: number = 5
 export const OFFLINE_PROGRESS: boolean = true
