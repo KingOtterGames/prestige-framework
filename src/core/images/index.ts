1fa1358a957e38d6a052f9554bdaebab2f182e8a
Brandon Pessman <pessman.brandon@gmail.com>
2024-06-02 09:42:28 -0500

    add image system

--- src/core/images/index.ts
+++ src/core/images/index.ts
@@ -1,1 +1,12 @@
-export const test = () => {}
+function importAll(r: any) {
+    let images = {}
+    r.keys().forEach((item: string) => {
+        ;(images as any)[item.replace('./', '')] = r(item)
+    })
+    return images
+}
+
+const images = importAll(require.context('../../assets/images/', true))
+export const get = (key: string) => {
+    return (images as any)[key]
+}
